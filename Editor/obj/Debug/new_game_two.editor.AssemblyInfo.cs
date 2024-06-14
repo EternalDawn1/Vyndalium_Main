72972119f42a0c1c7d5c280811ed28f437f83a61
Kyotoem <98539318+EternalDawn1@users.noreply.github.com>
2024-06-14 19:42:36 +0200

    removed hierachie

--- Editor/obj/Debug/new_game_two.editor.AssemblyInfo.cs
+++ Editor/obj/Debug/new_game_two.editor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("new_game_two.editor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99e6f2de9994136313904a8a1d14159018ccd8e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+652e9e733cb7f7f4a9b43b709a17626894ffeb8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("new_game_two.editor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("new_game_two.editor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
