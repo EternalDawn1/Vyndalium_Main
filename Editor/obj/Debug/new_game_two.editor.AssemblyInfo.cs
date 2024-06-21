46a1d509c553b32884fff5113663d1245b14ebf3
Kyotoem <98539318+EternalDawn1@users.noreply.github.com>
2024-06-21 17:52:34 +0200

    flickering lights added

--- Editor/obj/Debug/new_game_two.editor.AssemblyInfo.cs
+++ Editor/obj/Debug/new_game_two.editor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("new_game_two.editor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a307e6d7bcb8125cdb1f0a8cee30880fd7d44c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8eb0ae53311855b5105383e3f7117022651d88e")]
 [assembly: System.Reflection.AssemblyProductAttribute("new_game_two.editor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("new_game_two.editor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ code/FlickeringLight.cs
@@ -0,0 +1,9 @@
+using Sandbox;
+
+public sealed class FlickeringLight : Component
+{
+	protected override void OnUpdate()
+	{
+
+	}
+}
