577bc479f0d607376663687b0f0a2f4eeb0eed4a
Kyotoem <98539318+EternalDawn1@users.noreply.github.com>
2024-07-01 13:54:15 +0200

    added Chat Profile Icon

--- Libraries/facepunch.jigglebones/Code/obj/Debug/jigglebones.AssemblyInfo.cs
+++ Libraries/facepunch.jigglebones/Code/obj/Debug/jigglebones.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("jigglebones")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2dd83252d46cd3cae558826bf81ce3cf68859e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01a24e6681ee7627a798be6912172d38939adbd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("jigglebones")]
 [assembly: System.Reflection.AssemblyTitleAttribute("jigglebones")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
