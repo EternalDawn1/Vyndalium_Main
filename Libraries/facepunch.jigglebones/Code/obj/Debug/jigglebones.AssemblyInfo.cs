42db6f4b9ff643fa5aa91521342c0a7a52bef07d
Kyotoem <98539318+EternalDawn1@users.noreply.github.com>
2024-07-14 23:03:33 +0200

    added table textures

--- Libraries/facepunch.jigglebones/Code/obj/Debug/jigglebones.AssemblyInfo.cs
+++ Libraries/facepunch.jigglebones/Code/obj/Debug/jigglebones.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("jigglebones")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce7a7779dcf8941a4d524509f5251c2a8b7771a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+898aaf69e054c5fe3f6a4abc423439550448cf93")]
 [assembly: System.Reflection.AssemblyProductAttribute("jigglebones")]
 [assembly: System.Reflection.AssemblyTitleAttribute("jigglebones")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
