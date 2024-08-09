3c7519f67a2d1f114128ed670870291ba0165c15
Kyotoem <98539318+EternalDawn1@users.noreply.github.com>
2024-08-09 17:05:58 +0200

    removed some stuff

--- Editor/obj/Debug/vyndalium.editor.AssemblyInfo.cs
+++ Editor/obj/Debug/vyndalium.editor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("vyndalium.editor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3995bb3cd2c4cde208e3d23f78868a87ab86231")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc705bcac63223dfa8fc6a5952f5166ab74a6a9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("vyndalium.editor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("vyndalium.editor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
