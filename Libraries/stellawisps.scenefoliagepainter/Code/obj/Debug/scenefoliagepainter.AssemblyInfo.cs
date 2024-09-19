9913b6908133ba108a6824e3661d70ff4c6b620e
Kyotoem <98539318+EternalDawn1@users.noreply.github.com>
2024-09-19 12:49:15 +0200

    sell

--- Libraries/stellawisps.scenefoliagepainter/Code/obj/Debug/scenefoliagepainter.AssemblyInfo.cs
+++ Libraries/stellawisps.scenefoliagepainter/Code/obj/Debug/scenefoliagepainter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("scenefoliagepainter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8738151f9e829c2a2cccef598840a64d306f44a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a33819d92cfb7357bf92842d42e69cdb9715f098")]
 [assembly: System.Reflection.AssemblyProductAttribute("scenefoliagepainter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("scenefoliagepainter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
