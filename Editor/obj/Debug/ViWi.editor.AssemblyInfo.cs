bee70e5cba7ca8947a30e1539abc1d188a5f7d7d
Kyotoem <98539318+EternalDawn1@users.noreply.github.com>
2024-10-10 23:27:54 +0200

    save and sound

--- Editor/obj/Debug/ViWi.editor.AssemblyInfo.cs
+++ Editor/obj/Debug/ViWi.editor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ViWi.editor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8a02f4bf30ef80c8aa5accdcc1a79dac7575914")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6447107db1a43c04b4c83c378a7e6d3d58f8d5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ViWi.editor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ViWi.editor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
