81542f0d0f943252ac132c1bcbc96253db38dcd8
Kyotoem <98539318+EternalDawn1@users.noreply.github.com>
2024-10-18 14:07:05 +0200

    added anything to it

--- Editor/obj/Debug/ViWi.editor.AssemblyInfo.cs
+++ Editor/obj/Debug/ViWi.editor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ViWi.editor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+051d90b133f8277679c07478c0bb3b99780e6015")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c616a9b075caf8d252b167e839e0cbbd6003d9ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("ViWi.editor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ViWi.editor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
