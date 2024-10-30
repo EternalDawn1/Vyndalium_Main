5afa5aa9354e8e8b5ca58332b7ace56892213b4d
Kyotoem <98539318+EternalDawn1@users.noreply.github.com>
2024-10-30 10:04:07 +0100

    added 120 items + Assets

--- code/Chest/ItemStorage.cs
+++ code/Chest/ItemStorage.cs
@@ -130,6 +130,150 @@ namespace GeneralGame
 
       
 
+        private List<string> tierCPrefabs = new List<string>
+        {
+            "prefabs/weapons/aksu/c.prefab",
+            "prefabs/weapons/facepunch/usp/uspc.prefab",
+            "prefabs/weapons/facepunch/shotgun/shotgunc.prefab",
+            "prefabs/weapons/facepunch/mp5/mp5c.prefab",
+            "prefabs/weapons/m4a1/m4a1-c.prefab",
+            "prefabs/weapons/pm/glock-c.prefab",
+            "prefabs/clothes/armor/armor-c.prefab",
+            "prefabs/clothes/helmet/helmet-c.prefab",
+            "prefabs/clothes/legarmor/legarmor-c.prefab",
+            "prefabs/items/wood_log.prefab",
+            "prefabs/weapons/new/hands.prefab",
+            "prefabs/weapons/new/knife.prefab",
+            "prefabs/weapons/new/machete.prefab",
+            "prefabs/items/wood_log.prefab",
+            "prefabs/potions/potion_small.prefab",
+            "prefabs/potions/potion_mid.prefab",
+            "prefabs/potions/potion_big.prefab",
+            // Fügen Sie hier weitere C-Tier-Prefab-Dateien hinzu
+        };
+
+        private List<string> tierBPrefabs = new List<string>
+        {
+            "prefabs/weapons/aksu/b.prefab",
+            "prefabs/weapons/facepunch/usp/uspb.prefab",
+            "prefabs/weapons/facepunch/shotgun/shotgunb.prefab",
+            "prefabs/weapons/facepunch/mp5/mp5b.prefab",
+            "prefabs/weapons/m4a1/m4a1-b.prefab",
+            "prefabs/weapons/pm/glock-b.prefab",
+            "prefabs/clothes/armor/armor-b.prefab",
+            "prefabs/clothes/helmet/helmet-b.prefab",
+            "prefabs/clothes/legarmor/legarmor-b.prefab",
+            "prefabs/items/wood_log.prefab",
+             "prefabs/weapons/new/hands.prefab",
+            "prefabs/weapons/new/knife.prefab",
+            "prefabs/weapons/new/machete.prefab",
+            "prefabs/items/wood_log.prefab",
+            "prefabs/potions/potion_small.prefab",
+            "prefabs/potions/potion_mid.prefab",
+            "prefabs/potions/potion_big.prefab",
+
+            // Fügen Sie hier weitere B-Tier-Prefab-Dateien hinzu
+        };
+
+        private List<string> tierAPrefabs = new List<string>
+        {
+            "prefabs/weapons/aksu/a.prefab",
+            "prefabs/weapons/facepunch/usp/uspa.prefab",
+            "prefabs/weapons/facepunch/shotgun/shotguna.prefab",
+            "prefabs/weapons/facepunch/mp5/mp5a.prefab",
+            "prefabs/weapons/m4a1/m4a1-a.prefab",
+            "prefabs/weapons/pm/glock-a.prefab",
+            "prefabs/clothes/armor/armor-a.prefab",
+            "prefabs/clothes/helmet/helmet-a.prefab",
+            "prefabs/clothes/legarmor/legarmor-a.prefab",
+             "prefabs/weapons/new/hands.prefab",
+            "prefabs/weapons/new/knife.prefab",
+            "prefabs/weapons/new/machete.prefab",
+            "prefabs/items/wood_log.prefab",
+            "prefabs/potions/potion_small.prefab",
+            "prefabs/potions/potion_mid.prefab",
+            "prefabs/potions/potion_big.prefab",
+            // Fügen Sie hier weitere A-Tier-Prefab-Dateien hinzu
+        };
+
+        private List<string> tierSPrefabs = new List<string>
+        {
+            "prefabs/weapons/aksu/s.prefab",
+            "prefabs/weapons/facepunch/usp/usps.prefab",
+            "prefabs/weapons/facepunch/shotgun/shotguns.prefab",
+            "prefabs/weapons/facepunch/mp5/mp5s.prefab",
+            "prefabs/weapons/m4a1/m4a1-s.prefab",
+            "prefabs/weapons/pm/glock-s.prefab",
+            "prefabs/clothes/armor/armor-s.prefab",
+            "prefabs/clothes/helmet/helmet-s.prefab",
+            "prefabs/clothes/legarmor/legarmor-s.prefab",
+             "prefabs/weapons/new/hands.prefab",
+            "prefabs/weapons/new/knife.prefab",
+            "prefabs/weapons/new/machete.prefab",
+            "prefabs/items/wood_log.prefab",
+            "prefabs/potions/potion_small.prefab",
+            "prefabs/potions/potion_mid.prefab",
+            "prefabs/potions/potion_big.prefab",
+            // Fügen Sie hier weitere S-Tier-Prefab-Dateien hinzu
+        };
+
+        private List<string> tierSSPrefabs = new List<string>
+        {
+            "prefabs/weapons/aksu/ss.prefab",
+            "prefabs/weapons/facepunch/usp/uspss.prefab",
+            "prefabs/weapons/facepunch/shotgun/shotgunss.prefab",
+            "prefabs/weapons/facepunch/mp5/mp5ss.prefab",
+            "prefabs/weapons/m4a1/m4a1-ss.prefab",
+            "prefabs/weapons/pm/glock-ss.prefab",
+            "prefabs/clothes/armor/armor-ss.prefab",
+            "prefabs/clothes/helmet/helmet-ss.prefab",
+            "prefabs/clothes/legarmor/legarmor-ss.prefab",
+             "prefabs/weapons/new/hands.prefab",
+            "prefabs/weapons/new/knife.prefab",
+            "prefabs/weapons/new/machete.prefab",
+            "prefabs/items/wood_log.prefab",
+            "prefabs/potions/potion_small.prefab",
+            "prefabs/potions/potion_mid.prefab",
+            "prefabs/potions/potion_big.prefab",
+            // Fügen Sie hier weitere SS-Tier-Prefab-Dateien hinzu
+        };
+
+        private List<string> tierSSSPrefabs = new List<string>
+        {
+            "prefabs/weapons/aksu/sss.prefab",
+            "prefabs/weapons/facepunch/usp/uspsss.prefab",
+            "prefabs/weapons/facepunch/shotgun/shotgunsss.prefab",
+            "prefabs/weapons/facepunch/mp5/mp5sss.prefab",
+            "prefabs/weapons/m4a1/m4a1-sss.prefab",
+            "prefabs/weapons/pm/glock-sss.prefab",
+            "prefabs/clothes/armor/armor-sss.prefab",
+            "prefabs/clothes/helmet/helmet-sss.prefab",
+            "prefabs/clothes/legarmor/legarmor-sss.prefab",
+             "prefabs/weapons/new/hands.prefab",
+            "prefabs/weapons/new/knife.prefab",
+            "prefabs/weapons/new/machete.prefab",
+            "prefabs/items/wood_log.prefab",
+            "prefabs/potions/potion_small.prefab",
+            "prefabs/potions/potion_mid.prefab",
+            "prefabs/potions/potion_big.prefab",
+            // Fügen Sie hier weitere SSS-Tier-Prefab-Dateien hinzu
+        };
+        private List<string> tierUltimatePrefabs = new List<string>
+        {
+            "prefabs/weapons/aksu/sss.prefab",
+            "prefabs/weapons/facepunch/usp/uspsss.prefab",
+            "prefabs/weapons/facepunch/shotgun/shotgunsss.prefab",
+            "prefabs/weapons/facepunch/mp5/mp5sss.prefab",
+            "prefabs/weapons/m4a1/m4a1-sss.prefab",
+            "prefabs/weapons/pm/glock-sss.prefab",
+            "prefabs/clothes/armor/armor-sss.prefab",
+            "prefabs/clothes/helmet/helmet-sss.prefab",
+            "prefabs/clothes/legarmor/legarmor-sss.prefab",
+             "prefabs/weapons/new/hands.prefab",
+            "prefabs/weapons/new/knife.prefab",
+            "prefabs/weapons/new/machete.prefab",
+          
+        };
         
       
         private bool itemsLoaded = false;
@@ -176,11 +320,11 @@ namespace GeneralGame
             (bossItems, "SSS", 0.03),
             (bossItems, "Ultimate", 0.01),
 
-            (basePrefabs, "A", 0.50),  // 10%
-            (basePrefabs, "S", 0.20),  // 10%
-            (basePrefabs, "SS", 0.10), // 5%
-            (basePrefabs, "SSS", 0.05),
-            (basePrefabs, "Ultimate", 0.01) // 5%
+            (tierSPrefabs, "A", 0.50),  // 10%
+            (tierSPrefabs, "S", 0.20),  // 10%
+            (tierSSPrefabs, "SS", 0.10), // 5%
+            (tierSSSPrefabs, "SSS", 0.005),
+            (tierUltimatePrefabs, "Ultimate", 0.001) // 5%
             };
             int itemsToSpawn;
 
@@ -292,7 +436,7 @@ namespace GeneralGame
                 (basePrefabs, "S", 0.08),
                 (basePrefabs, "SS", 0.05),
                 (basePrefabs, "SSS", 0.01),
-                
+                (basePrefabs, "Ultimate", 0.001),
                 
             }; 
             int itemsToSpawn;
