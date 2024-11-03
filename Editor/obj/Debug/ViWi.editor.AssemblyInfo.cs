b7526bfb7687b73631027e9d380a52661892a115
Kyotoem <98539318+EternalDawn1@users.noreply.github.com>
2024-11-03 19:56:59 +0100

    rehaul player ui

--- Editor/obj/Debug/ViWi.editor.AssemblyInfo.cs
+++ Editor/obj/Debug/ViWi.editor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ViWi.editor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25c10ba0b0d5cfb2ad80b1cfe716467e0fa29d14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcec80eeb3672c5e7dc02946a7fdd5f4e6ffa01a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ViWi.editor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ViWi.editor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
